4281020700cdf809dcc07d68118eaef086b30359
Collin Kreklow <collin@kreklow.us>
2019-06-16 16:11:22 -0500

    Add beast package
    The beast package supports decoding ADS-B messages in Mode-S Beast
    format. The package includes a Decoder which is suitable for wrapping a
    net.Conn, as would be useful for connecting to a dump1090 instance.

---
+++ beast/beast.go
@@ -0,0 +1,164 @@
+// Copyright 2019 Collin Kreklow
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Package beast provides objects and methods for decoding and
+// managing raw Mode-S Beast format frames.
+//
+package beast
+
+import (
+	"bufio"
+	"encoding"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+
+	"kreklow.us/go/go-adsb/adsb"
+)
+
+// Frame is a Mode-S Beast format message
+type Frame struct {
+	raw []byte
+
+	Format    uint8 // 1: Mode-AC, 2: Short Mode-S, 3: Long Mode-S, 4: Status
+	Timestamp uint64
+	Signal    uint8
+
+	Msg *adsb.Message
+}
+
+// UnmarshalBinary accepts an unescaped Beast frame and populates the
+// fields of the Frame object.
+func (f *Frame) UnmarshalBinary(data []byte) error {
+	if data[0] != 0x1a {
+		return errors.New("format identifier not found")
+	}
+	switch data[1] {
+	case 0x32:
+		if len(data) != 16 {
+			return fmt.Errorf("expected 16 bytes, received %d", len(data))
+		}
+		f.raw = data
+		f.Format = 2
+	case 0x33:
+		if len(data) != 23 {
+			return fmt.Errorf("expected 23 bytes, received %d", len(data))
+		}
+		f.raw = data
+		f.Format = 3
+	case 0x31, 0x34:
+		return errors.New("format not supported")
+	default:
+		return errors.New("invalid format identifier")
+	}
+	f.Timestamp = binary.BigEndian.Uint64(append([]byte{0, 0}, f.raw[2:8]...))
+	f.Signal = f.raw[8]
+
+	f.Msg = new(adsb.Message)
+	err := f.Msg.Decode(f.raw[9:])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Decoder reads and decodes a Beast stream
+type Decoder struct {
+	r *bufio.Reader
+}
+
+// NewDecoder returns a Decoder which reads from r.
+func NewDecoder(r io.Reader) *Decoder {
+	d := new(Decoder)
+	d.r = bufio.NewReader(r)
+	return d
+}
+
+// Decode reads the next Beast frame from the input source and stores it
+// in f. The UnmarshalBinary method of f must support unescaped Beast
+// format data.
+func (d *Decoder) Decode(f encoding.BinaryUnmarshaler) error {
+	m := make([]byte, 0, 64)
+
+	b, err := d.r.ReadByte()
+	if err != nil {
+		return err
+	}
+	if b != 0x1a {
+		return errors.New("data stream corrupt")
+	}
+
+	m = append(m, b)
+
+	b, err = d.r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	var l int
+
+	switch b {
+	case 0x31:
+		l = 11
+	case 0x32:
+		l = 16
+	case 0x33:
+		l = 23
+	default:
+		return errors.New("unsupported frame type")
+	}
+
+	m = append(m, b)
+
+	for j := len(m); j < l; j = len(m) {
+		b, err = d.r.ReadByte()
+		if err != nil {
+			return err
+		}
+
+		if b == 0x1a {
+			nb, err := d.r.Peek(1)
+			if err != nil {
+				return err
+			}
+
+			if nb[0] == 0x1a {
+				_, err = d.r.Discard(1)
+				if err != nil {
+					return err
+				}
+			} else {
+				return errors.New("frame truncated")
+			}
+		}
+		m = append(m, b)
+	}
+
+	err = f.UnmarshalBinary(m)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ beast/beast_test.go
@@ -0,0 +1,189 @@
+// Copyright 2019 Collin Kreklow
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package beast
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
+
+func TestUnmarshalBadData(t *testing.T) {
+	testUnmarshalError(t, "ff0000ff", "format identifier not found")
+}
+
+func TestUnmarshalBadLength2(t *testing.T) {
+	testUnmarshalError(t, "1a32ffff", "expected 16 bytes, received 4")
+}
+
+func TestUnmarshalBadLength3(t *testing.T) {
+	testUnmarshalError(t, "1a33ffff", "expected 23 bytes, received 4")
+}
+
+func TestUnmarshalType1(t *testing.T) {
+	testUnmarshalError(t, "1a31ffff", "format not supported")
+}
+
+func TestUnmarshalType4(t *testing.T) {
+	testUnmarshalError(t, "1a34ffff", "format not supported")
+}
+
+func TestUnmarshalBadType(t *testing.T) {
+	testUnmarshalError(t, "1affffff", "invalid format identifier")
+}
+
+func TestUnmarshalBadDecode(t *testing.T) {
+	testUnmarshalError(t, "1a32ffffffffffffffffffffffffffff", "unsupported format: 31")
+}
+
+func testUnmarshalError(t *testing.T, msg string, e string) {
+	f := new(Frame)
+	b, err := hex.DecodeString(msg)
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+	err = f.UnmarshalBinary(b)
+	if err == nil {
+		t.Errorf("expected %s, received nil", e)
+	} else if err.Error() != e {
+		t.Errorf("expected %s, received %s", e, err.Error())
+	}
+}
+
+type testCase struct {
+	Msg string
+
+	Format    uint8
+	Timestamp uint64
+	Signal    uint8
+
+	DF   int
+	TC   int
+	ICAO string
+}
+
+func TestDecode2(t *testing.T) {
+	tc := &testCase{
+		Msg:       "1a3216f933baf325c45da99adad95ff6",
+		Format:    2,
+		Timestamp: 25259570557733,
+		Signal:    196,
+		DF:        11,
+		TC:        -1,
+		ICAO:      "a99ada",
+	}
+	testDecoder(t, tc)
+}
+
+func TestDecode3(t *testing.T) {
+	tc := &testCase{
+		Msg:       "1a3316f933bbc63ec68da99ada58b98446e703357e2417",
+		Format:    3,
+		Timestamp: 25259570611774,
+		Signal:    198,
+		DF:        17,
+		TC:        11,
+		ICAO:      "a99ada",
+	}
+	testDecoder(t, tc)
+}
+
+func testDecoder(t *testing.T, tc *testCase) {
+	b, err := hex.DecodeString(tc.Msg)
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+	r := bytes.NewReader(b)
+	d := NewDecoder(r)
+	f := new(Frame)
+	err = d.Decode(f)
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	if tc.Format != f.Format {
+		t.Errorf("Format: expected %d, received %d", tc.Format, f.Format)
+	}
+	if tc.Timestamp != f.Timestamp {
+		t.Errorf("Timestamp: expected %d, received %d", tc.Timestamp, f.Timestamp)
+	}
+	if tc.Timestamp != f.Timestamp {
+		t.Errorf("Timestamp: expected %d, received %d", tc.Timestamp, f.Timestamp)
+	}
+	if tc.Signal != f.Signal {
+		t.Errorf("Signal: expected %d, received %d", tc.Signal, f.Signal)
+	}
+	if tc.DF != int(f.Msg.DF) {
+		t.Errorf("DF: expected %d, received %d", tc.DF, f.Msg.DF)
+	}
+	if tc.TC != int(f.Msg.TC) {
+		t.Errorf("TC: expected %d, received %d", tc.TC, f.Msg.TC)
+	}
+	if tc.ICAO != f.Msg.ICAO {
+		t.Errorf("ICAO: expected %s, received %s", tc.ICAO, f.Msg.ICAO)
+	}
+}
+
+func TestDecodeNull(t *testing.T) {
+	testDecoderError(t, "", "EOF")
+}
+
+func TestDecodeShort1(t *testing.T) {
+	testDecoderError(t, "1a", "EOF")
+}
+
+func TestDecodeShort2(t *testing.T) {
+	testDecoderError(t, "1a31", "EOF")
+}
+
+func TestDecodeShort3(t *testing.T) {
+	testDecoderError(t, "1a331a", "EOF")
+}
+
+func TestDecodeBadStart(t *testing.T) {
+	testDecoderError(t, "ff00", "data stream corrupt")
+}
+
+func TestDecodeTruncated(t *testing.T) {
+	testDecoderError(t, "1a32ffff1a33ff", "frame truncated")
+}
+
+func TestDecodeUnsupported(t *testing.T) {
+	testDecoderError(t, "1a31ffffffffffffffffff", "format not supported")
+}
+
+func testDecoderError(t *testing.T, msg string, e string) {
+	b, err := hex.DecodeString(msg)
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+	r := bytes.NewReader(b)
+	d := NewDecoder(r)
+	f := new(Frame)
+	err = d.Decode(f)
+	if err == nil {
+		t.Errorf("expected %s, received nil", e)
+	} else if err.Error() != e {
+		t.Errorf("expected %s, received %s", e, err.Error())
+	}
+}
